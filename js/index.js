092b814c13a722ebd6d23e08dcbbf8ebed443de0
Paramount2009 <Paramount7891@gmail.com>
2016-03-03 16:19:01 +0300

    height mobile

--- js/index.js
+++ js/index.js
@@ -51,5 +51,9 @@ $(document).scroll(function(){
 
 });
 $(".down-page").click(function(){scrollFront();});
-
+/////////////////////////////////////////////////////set for mobile height
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+	var heightViewport = window.innerHeight;
+ 	$(".front-main").css('height', heightViewport + " !important");
+}
 });
